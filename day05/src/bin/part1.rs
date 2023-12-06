83c737c71bc6fed0c02bc5a545be20dd920815bf
LudwigZeller <91737401+LudwigZeller@users.noreply.github.com>
2023-12-06 22:04:46 +0100

    Day 05 - Progress

--- day05/src/bin/part1.rs
+++ day05/src/bin/part1.rs
@@ -1,15 +1,37 @@
-use nom::*;
+use nom::{
+    bytes::complete::{tag, take_until1},
+    multi::many0,
+    sequence::terminated,
+    IResult,
+};
+
 fn main() {
     let data = include_str!("data");
     let result = process(data);
     dbg!(result);
 }
 
-fn parse_file(data: &str) -> IResult<&str, &str> {
-    todo!()
+struct Translator<'a> {
+    id: (&'a str, &'a str),
+    input: Vec<(usize, usize)>,
+}
+
+impl<'a> From<&'a str> for Translator<'a> {
+    fn from(value: &'a str) -> Self {
+        todo!()
+    }
+}
+
+fn parse_multiline_block(data: &str) -> IResult<&str, &str> {
+    terminated(take_until1("\n\n"), tag("\n\n"))(data)
+}
+
+fn parse_all_multiline_blocks(data: &str) -> IResult<&str, Vec<&str>> {
+    many0(parse_multiline_block)(data)
 }
 
 fn process(data: &str) -> usize {
+    dbg!(parse_all_multiline_blocks(data).unwrap().1);
     todo!()
 }
 
@@ -18,16 +40,39 @@ mod tests {
     fn part1() {
         assert_eq!(
             super::process(
-                "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598.."
+                "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"
             ),
             4361
         )
