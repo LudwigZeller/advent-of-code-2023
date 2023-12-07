d0ac72562a9930be54acb4250cc041d9935a1fa2
LudwigZeller <91737401+LudwigZeller@users.noreply.github.com>
2023-12-07 23:07:12 +0100

    Day 03 - Cleanup

--- day03/src/bin/part1.rs
+++ day03/src/bin/part1.rs
@@ -1,4 +1,3 @@
-use nom::{IResult, complete::tag,}
 fn main() {
     let data = include_str!("data");
     let result = process(data);
@@ -13,26 +12,10 @@ enum Field {
 
 fn parse(line: &str) -> Vec<Field> {
     todo!()
-    //     let mut vec = Vec::with_capacity(line.len());
-    //     for (inx, char) in line.chars().enumerate() {
-    //         vec.push(match char {
-    //             '.' => Field::Empty,
-    //             char if char.is_ascii_digit() => {
-    //                 todo!()
-    //             }
-    //             char => Field::Cog(char),
-    //         });
-    //     }
-    //     vec
 }
 
 fn process(data: &str) -> usize {
-    let a = data
-        .split('\n')
-        .filter(|line| !line.is_empty())
-        .map(parse)
-        .collect::<Vec<Vec<Field>>>();
-    todo!()
+    data.split('\n').filter(|line| !line.is_empty()).map(parse(jjk))
 }
 
 mod tests {
@@ -40,18 +23,14 @@ mod tests {
     fn part1() {
         assert_eq!(
             super::process(
-                "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598.."
+                "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
             ),
-            4361
+            13
         )
     }
 }
